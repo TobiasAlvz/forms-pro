b43f875990fb58ecd97d21cd5383bdf400b94094
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-06 16:42:02 -0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { useTheme } from "@/themes/ThemeContext";
+import { Stack } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
+  const { theme, switchTheme } = useTheme();
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: theme.colors.background,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: theme.colors.surface },
+          headerTintColor: theme.colors.text,
+        }}
+      />
+      <Text style={{ color: theme.colors.text, fontSize: theme.fontSizes.lg }}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+      <Button
+        title="Switch theme"
+        color={theme.colors.primary}
+        onPress={() => switchTheme()}
+      />
     </View>
   );
 }
