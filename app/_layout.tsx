7dee3c9cc02c92f79e2bd1845db41814abb416b9
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-09 13:44:31 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "@/themes/ThemeContext";
 import { Stack } from "expo-router";
 
+// https://complex-humor-1df.notion.site/302cc8c2db9680e7be47cdd21ae9bff3
+
 export default function RootLayout() {
   return (
     <ThemeProvider>
