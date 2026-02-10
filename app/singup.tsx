74ca5608170e4c226533d01b83f18151deecaee1
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-10 16:38:29 -0300

    Update singup.tsx

--- app/singup.tsx
+++ app/singup.tsx
@@ -1,11 +1,12 @@
 import { ScreenContainer } from "@/components/ScreenContainer";
+import { SingUpForm } from "@/components/SingUpForm";
 import { Title } from "@/components/Title";
 
 export default function SingUpScreen() {
   return (
     <ScreenContainer>
       <Title align="center">Crie sua Conta</Title>
-      {/* <SingUpForm/> */}
+      <SingUpForm/>
     </ScreenContainer>
   );
 }
