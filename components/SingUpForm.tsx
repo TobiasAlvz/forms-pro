f2df0d2e907f230477851abac69eef57dd9086fd
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-10 16:38:27 -0300

    Update SingUpForm.tsx

---
+++ components/SingUpForm.tsx
@@ -0,0 +1,63 @@
+import { useTheme } from "@/themes/ThemeContext";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { Alert, View } from "react-native";
+import { Input } from "./input";
+import { Button } from "./Button";
+
+export const SingUpForm = () => {
+  const { theme } = useTheme();
+  const router = useRouter();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSingUp = () => {
+    if (password !== confirmPassword) {
+      Alert.alert("Error", "As senhas não são iguais");
+      return;
+    }
+    //   aqui vai ser o registro do supabase;
+  };
+
+  return (
+    <>
+      <View>
+        <Input placeholder="Nome" value={name} onChangeText={setName}></Input>
+
+        <Input
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
+        ></Input>
+
+        <Input
+          placeholder="Telefone"
+          keyboardType="phone-pad"
+          value={phone}
+          onChangeText={setPhone}
+        ></Input>
+
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        ></Input>
+
+        <Input
+          placeholder="Confirm Password"
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        ></Input>
+      </View>
+      <Button title="Cadastrar" onPress={handleSingUp} />
+    </>
+  );
+};
