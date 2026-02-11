5705855bfc2bbb55c37a5e32c115e802e7bc1cbc
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-11 15:06:10 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,11 @@ import { ThemeProvider } from "@/themes/ThemeContext";
 export default function RootLayout() {
   return (
     <ThemeProvider>
-      <Stack />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="singup" options={{ title: "Cadastrar Cliente" }} />
+        <Stack.Screen name="confirm-email" options={{ headerShown: false }} />
+      </Stack>
     </ThemeProvider>
   );
 }
