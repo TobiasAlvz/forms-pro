188329f7165830759f835fff95bf26c19477a40e
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-11 17:02:16 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,20 @@
 import { Stack } from "expo-router";
 import { ThemeProvider } from "@/themes/ThemeContext";
+import { SessionProvider } from "@/providers/SessionContext";
 
 export default function RootLayout() {
   return (
     <ThemeProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="singup" options={{ title: "Cadastrar Cliente" }} />
-        <Stack.Screen name="confirm-email" options={{ headerShown: false }} />
-      </Stack>
+      <SessionProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="singup"
+            options={{ title: "Cadastrar Cliente" }}
+          />
+          <Stack.Screen name="confirm-email" options={{ headerShown: false }} />
+        </Stack>
+      </SessionProvider>
     </ThemeProvider>
   );
 }
