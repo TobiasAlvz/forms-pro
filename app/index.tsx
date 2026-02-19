afec93ec8f1b374b3e5438e234a817c1f612df5f
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-19 13:31:15 -0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -7,14 +7,13 @@ import { SignInForm } from "@/components/SingInForm";
 import { useSession } from "@/providers/SessionContext";
 import { useEffect } from "react";
 
-//https://www.notion.so/304cc8c2db96809aad0be0827978f4d6
+
 
 export default function Index() {
   const { theme } = useTheme();
   const styles = createStyles(theme);
   const { user, session } = useSession();
   const router = useRouter();
-
 
   useEffect(() => {
     if (session) {
