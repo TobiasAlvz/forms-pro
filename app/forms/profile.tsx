35e5573fd50e91645dc67d6f36e9074caa1d36f6
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-25 14:33:46 -0300

    Create profile.tsx

---
+++ app/forms/profile.tsx
@@ -0,0 +1,13 @@
+import { UpdateProfileSection } from "@/components/profile/UpdateProfileSection";
+import { ScreenContainer } from "@/components/ScreenContainer";
+import { Text } from "react-native";
+
+export default function ProfileScreen() {
+  return (
+    <ScreenContainer>
+      <Text></Text>
+      <UpdateProfileSection/>
+            {/* <UpdatePasswordSection/> */}
+    </ScreenContainer>
+  );
+}
