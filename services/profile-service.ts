430ad705a39866a3ed745b53dbeadfe2d935692e
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-25 13:27:44 -0300

    Update profile-service.ts

--- services/profile-service.ts
+++ services/profile-service.ts
@@ -1,14 +1,14 @@
 import { supabase } from "@/lib/supabase";
 import { Alert } from "react-native";
 
-type userProfile = {
+type UserProfile = {
   name: string;
   email: string;
   phone: string;
 };
 
 const profileService = {
-  updateProfile: async (updates: Partial<userProfile>) => {
+  updateProfile: async (updates: Partial<UserProfile>) => {
     const payload: {
       email?: string;
       data: Record<string, string>;
@@ -22,9 +22,27 @@ const profileService = {
 
     if (error) {
       console.log(error);
-      Alert.alert("Erro", "Erro");
+      Alert.alert(
+        "Erro ao atualizar",
+        "Não foi possível atualizar seus dados. Tente novamente.",
+      );
       return;
     }
     return data.user;
   },
+
+  updatePassword: async (newPassword: string) => {
+    const { error } = await supabase.auth.updateUser({ password: newPassword });
+
+    if (error) {
+      console.log(error);
+      Alert.alert(
+        "Erro ao alterar senha",
+        "Não foi possível atualizar sua senha. Verifique e tente novamente.",
+      );
+      return;
+    }
+  },
 };
+
+export default profileService;
