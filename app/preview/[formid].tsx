1c8a33a333d50467481342e120c730a079e63d94
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-02-26 14:28:51 -0300

    Update [formid].tsx

--- app/preview/[formid].tsx
+++ app/preview/[formid].tsx
@@ -1,7 +1,58 @@
+import { ScreenContainer } from "@/components/ScreenContainer";
+import { Title } from "@/components/Title";
+import publicFormService, {
+  PublicFormWithFields,
+} from "@/services/public-form-service";
+import { useTheme } from "@/themes/ThemeContext";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 export default function PreviewFormsScreen() {
   const params = useLocalSearchParams();
-  const formId = Array.isArray(params.formId);
-  
+  const formId = Array.isArray(params.formId)
+    ? params.formId[0]
+    : params.formId;
+
+  const { theme } = useTheme();
+  //   const styles = createStyles(theme);
+
+  const [form, setForm] = useState<PublicFormWithFields | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!formId) return;
+    const load = async () => {
+      const data = await publicFormService.getFormWithFields(formId, {
+        preview: true,
+      });
+      if (!data) {
+        Alert.alert("Erro", "Formulario não encontrado");
+        return;
+      }
+      setForm(data);
+      setLoading(false);
+    };
+    load();
+  }, [formId]);
+
+  if (loading) {
+    return (
+      <ScreenContainer>
+        <Title>Carrengando...</Title>
+      </ScreenContainer>
+    );
+  }
+
+  return (
+    <ScreenContainer>
+      <Title>{form?.title}</Title>
+      {!!form?.description && <Text>{form.description}</Text>}
+
+      <View>
+        <Text>Visualização de Formulario</Text>
+        <Text>Campos de respostas</Text>
+      </View>
+    </ScreenContainer>
+  );
 }
